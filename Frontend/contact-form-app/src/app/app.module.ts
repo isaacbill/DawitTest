67034ede13209f831aa927ed45f1ea9551604c9a
isaac bill <isaacbill247@gmail.com>
2024-05-06 15:09:29 +0300

    contact form Component

--- Frontend/contact-form-app/src/app/app.module.ts
+++ Frontend/contact-form-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
@@ -10,7 +11,9 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     ContactFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
